c7a911e84c34a5e06168a2c174df78185bd6f418
woody <45289944+woody21978@users.noreply.github.com>
2020-05-07 18:36:04 +0300

    Перезалив

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
+const fs = require("fs");
+
 
 // для того, чтобы можно было обрабатывать req.body
 app.use(bodyParser.text());
@@ -15,10 +17,12 @@ app.get('/users', function (req, res) {
   /*
     На данном маршруте нужно вывести содержимое файла users.dat
   */
- 
+  let rawdata = fs.readFileSync('users.data', 'utf-8');
+  let student = JSON.parse(rawdata);
+  console.log(student);
   res.send('Маршрут для вывод содержимого файла пользователей');
 });
- 
+
 app.post('/user-register', function (req, res) {
   /*
     На данном маршруте нужно добавить пользователя (по POST запросу):
@@ -28,6 +32,15 @@ app.post('/user-register', function (req, res) {
   */
   console.log('Пришли данные с клиента', req.body);
   res.send('Маршрут для регистрации пользователя');
+  let check_file = fs.readFileSync('users.data');
+  if (check_file == "") {
+    fs.writeFile("users.data", JSON.stringify(req.body), function (error) {
+      if (error) throw error; // если возникла ошибка
+    });
+  } else {
+    let newData = check_file + ", \n" + JSON.stringify(req.body);
+    fs.writeFileSync('users.data', newData);
+  }
 });
 
 app.listen(80, (err) => {
