d3381e216fa9e80774b70901528f31460dc8ec5c
woody <45289944+woody21978@users.noreply.github.com>
2020-05-07 18:27:34 +0300

    перезалив

--- app.js
+++ app.js
@@ -1,47 +1,22 @@
-document.querySelector(".registration-form").onsubmit = function (event) {
+document.querySelector(".registration-form").onsubmit = function(event) {
   event.preventDefault();
   const username = event.target.querySelector('input[name="username"]').value;
   const email = event.target.querySelector('input[name="email"]').value;
-  console.log("submit", username, email);
   const data = {
     username: username,
     email: email
   }
+  // console.log("submit", username, email);
+  let json = JSON.stringify(data);
+  console.log(json);
 
-  fetch('http://localhost/user-register', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8'
-    },
-    body: JSON.stringify(data)
-  });
+  
 
   // TODO: сделайте POST запрос на ваш сервер для сохранения пользователя
   // пример маршрута POST /user
 };
 
-window.onload = function () {
+window.onload = function() {
   // TODO: GET запрос (GET /users)
   // полученные данные выведите в DOM (.user-list)
-  // ${formattedJson}
-  let users = document.querySelector('.user-list'),
-    user = document.createElement('div'),
-    name = document.createElement('strong'),
-    email = document.createElement('div');
-  user.classList.add('user');
-  name.innerHTML;
-  fetch('http://localhost:80/users')
-    .then(function (res) {
-      if (res.status === 200 && res.ok) {
-        res.json().then(result => {
-          console.log(true);
-        });
-      }
-    })
-    .catch(function (res) {
-      console.log(res);
-    });
-  user.appendChild(name);
-  user.appendChild(email);
-  users.appendChild(user);
 };
--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
-const fs = require("fs");
-
 
 // для того, чтобы можно было обрабатывать req.body
 app.use(bodyParser.text());
@@ -17,12 +15,10 @@ app.get('/users', function (req, res) {
   /*
     На данном маршруте нужно вывести содержимое файла users.dat
   */
-  let rawdata = fs.readFileSync('users.data', 'utf-8');
-  let student = JSON.parse(rawdata);
-  console.log(student);
+ 
   res.send('Маршрут для вывод содержимого файла пользователей');
 });
-
+ 
 app.post('/user-register', function (req, res) {
   /*
     На данном маршруте нужно добавить пользователя (по POST запросу):
@@ -32,15 +28,6 @@ app.post('/user-register', function (req, res) {
   */
   console.log('Пришли данные с клиента', req.body);
   res.send('Маршрут для регистрации пользователя');
-  let check_file = fs.readFileSync('users.data');
-  if (check_file == "") {
-    fs.writeFile("users.data", JSON.stringify(req.body), function (error) {
-      if (error) throw error; // если возникла ошибка
-    });
-  } else {
-    let newData = check_file + ", \n" + JSON.stringify(req.body);
-    fs.writeFileSync('users.data', newData);
-  }
 });
 
 app.listen(80, (err) => {
